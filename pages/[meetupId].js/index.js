17f0b2c897d107f729b4f6fb7f67a9ff771121f7
shalnsingh@outlook.com <shalnsingh@outlook.com>
2022-04-18 23:52:10 +0530

    updating styles

---
+++ pages/[meetupId].js/index.js
@@ -0,0 +1,15 @@
+
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+const MeetupDetails=()=>{
+ return (
+     <MeetupDetail
+     image='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/1024px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG'
+     title='First Meetup'
+     address='Some Street 5, Some City'
+     description='This is a first meetup'/>
+ )
+
+}
+
+export default MeetupDetails;
